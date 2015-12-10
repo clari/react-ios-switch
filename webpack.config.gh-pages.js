2e06adf17e49201464a47b5b753fb16bb8a78068
Matthew King <mking-clari@users.noreply.github.com>
2015-12-10 13:52:24 -0800

    optimize gh-pages

--- webpack.config.gh-pages.js
+++ webpack.config.gh-pages.js
@@ -32,7 +32,8 @@ module.exports = {
   plugins: [
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production')
-    })
+    }),
+    new webpack.optimize.UglifyJsPlugin()
   ],
   postcss: [autoprefixer]
 }
