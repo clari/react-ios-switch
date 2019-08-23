e0d6dcf1aeac4145d9c49d569184a153e9cb46ad
Jean Luis Urena <jlurena@teladochealth.com>
2019-08-23 09:54:44 -0400

    Make toggle switch accessible

--- src/Switch.jsx
+++ src/Switch.jsx
@@ -7,6 +7,7 @@ import prefixStyle from './prefixStyle';
 
 export default class Switch extends React.Component {
   static propTypes = {
+    ariaLabel: PropTypes.string,
     checked: PropTypes.bool,
     className: PropTypes.string,
     disabled: PropTypes.bool,
@@ -18,27 +19,29 @@ export default class Switch extends React.Component {
     pendingOffColor: PropTypes.string,
     pendingOnColor: PropTypes.string,
     readOnly: PropTypes.bool,
-    style: PropTypes.object,
+    style: PropTypes.object
   };
 
   static defaultProps = {
+    ariaLabel: 'Switch',
     handleColor: 'white',
     offColor: 'white',
     onChange: () => {},
-    onColor: 'rgb(76, 217, 100)',
+    onColor: 'rgb(76, 217, 100)'
   };
-  
+
   constructor(props) {
     super(props);
-    
+
     this.state = {
       isDragging: false,
-      offset: null,
+      offset: null
     };
-    
+
     this.handleChange = this.handleChange.bind(this);
     this.handleClick = this.handleClick.bind(this);
     this.handleHandleClick = this.handleHandleClick.bind(this);
+    this.handleOnKeyDown = this.handleOnKeyDown.bind(this);
     this.handleMouseDown = this.handleMouseDown.bind(this);
     this.handleMouseUp = this.handleMouseUp.bind(this);
     this.setRef = this.setRef.bind(this);
@@ -60,11 +63,11 @@ export default class Switch extends React.Component {
   componentWillUnmount() {
     window.removeEventListener('mouseup', this.handleMouseUp);
   }
-  
+
   getHandleColor() {
     return normalizeColor(this.props.handleColor);
   }
-  
+
   getHandleCursor() {
     if (this.isDisabled()) {
       return 'default';
@@ -76,7 +79,7 @@ export default class Switch extends React.Component {
   getHandleLength() {
     return this.getHeight() - 2;
   }
-  
+
   getHeight() {
     return 30;
   }
@@ -89,31 +92,28 @@ export default class Switch extends React.Component {
     if (this.state.isDragging) {
       return this.state.offset;
     }
-    
+
     return this.props.checked ? this.getOffsetWidth() : 0;
   }
-  
+
   getOffsetProgress() {
     return this.getOffset() / this.getOffsetWidth();
   }
-  
+
   getOffsetWidth(props) {
-    return (this.getWidth() - this.getHandleLength()) - 2;
+    return this.getWidth() - this.getHandleLength() - 2;
   }
 
   getOnColor() {
     return normalizeColor(this.props.onColor);
   }
 
-  getPendingColor({
-    color,
-    pendingColor,
-  }) {
+  getPendingColor({ color, pendingColor }) {
     if (!pendingColor) {
-      return color === 'white' ? 
-        // default pending color for white color
-        '#dfdfdf' :
-        normalizeColor(color);
+      return color === 'white'
+        ? // default pending color for white color
+          '#dfdfdf'
+        : normalizeColor(color);
     }
 
     return normalizeColor(pendingColor);
@@ -122,21 +122,21 @@ export default class Switch extends React.Component {
   getPendingOffColor() {
     return this.getPendingColor({
       color: this.props.offColor,
-      pendingColor: this.props.pendingOffColor,
+      pendingColor: this.props.pendingOffColor
     });
   }
 
   getPendingOnColor() {
     return this.getPendingColor({
       color: this.props.onColor,
-      pendingColor: this.props.pendingOnColor,
+      pendingColor: this.props.pendingOnColor
     });
   }
-  
+
   getWidth() {
     return 50;
   }
-  
+
   handleChange(e) {
     this.props.onChange(e.target.checked);
   }
@@ -149,7 +149,7 @@ export default class Switch extends React.Component {
     // handle case when the switch is clicked
     this.clickChange(!this.props.checked);
   }
-  
+
   handleHandleClick(e) {
     e.stopPropagation();
   }
@@ -158,127 +158,139 @@ export default class Switch extends React.Component {
     if (this.isDisabled()) {
       return;
     }
-  
+
     this.pointerTracker = pointer(e).start();
-    
+
     this.offsetTracker = trackOffset(this.pointerTracker.x, {
       from: this.getOffset(),
       onUpdate: transform.pipe(
         transform.clamp(0, this.getOffsetWidth()),
         offset => this.setState({ offset })
-      ),
+      )
     }).start();
-    
+
     this.setState({
       isDragging: true,
-      offset: this.getOffset(),
+      offset: this.getOffset()
     });
   }
-  
+
   handleMouseUp() {
     if (!this.state.isDragging) {
       return;
     }
-  
+
     this.pointerTracker.stop();
     this.offsetTracker.stop();
-    
-    const prevOffset = this.props.checked ? this.getOffsetWidth() : 0;
-    const checked = this.state.offset === prevOffset ?
-      // handle case when the handle is clicked
-      !this.props.checked :
-      // handle case when the handle is dragged
-      this.getOffsetProgress() >= 0.5;
 
-    this.setState({ 
+    const prevOffset = this.props.checked ? this.getOffsetWidth() : 0;
+    const checked =
+      this.state.offset === prevOffset
+        ? // handle case when the handle is clicked
+          !this.props.checked
+        : // handle case when the handle is dragged
+          this.getOffsetProgress() >= 0.5;
+
+    this.setState({
       isDragging: false,
-      offset: null,
+      offset: null
     });
 
     this.clickChange(checked);
   }
-  
+
+  handleOnKeyDown(e) {
+    if (e.key === ' ' || e.key === 'Enter') {
+      this.handleClick(e);
+      e.preventDefault();
+      e.stopPropagation();
+    }
+  }
+
   isDisabled() {
     return this.props.disabled || this.props.readOnly;
   }
 
   render() {
-    const { 
-      checked,
-      className,
-      disabled,
-      name,
-      onChange,
-      readOnly,
-      style,
-    } = this.props;
-            
+    const { ariaLabel, checked, className, disabled, name, readOnly, style } = this.props;
+
     const { isDragging } = this.state;
-            
+
     const color = transform.pipe(
       easing.createExpoIn(2),
       transform.blendColor(this.getOffColor(), this.getOnColor()),
-      transform.rgba,
+      transform.rgba
     )(this.getOffsetProgress());
-    
+
     const borderColor = transform.pipe(
       easing.createExpoIn(1),
       transform.blendColor(this.getPendingOffColor(), this.getPendingOnColor()),
-      transform.rgba,
+      transform.rgba
     )(this.getOffsetProgress());
-    
+
     return (
-      <span
+      <div
+        aria-checked={checked}
+        aria-label={ariaLabel}
+        aria-readonly={readOnly}
         className={className}
-        onClick={this.handleClick}
-        ref={this.setRef}
-        style={{
-          ...prefixStyle({
-            backgroundColor: color,
-            border: `1px solid ${borderColor}`,
-            borderRadius: this.getHeight() / 2,
-            boxShadow: `inset 0 0 0 ${this.getOffset()}px ${borderColor}`,
-            boxSizing: 'border-box',
-            display: 'inline-block',
-            height: this.getHeight(),
-            opacity: this.isDisabled() ? 0.5 : 1,
-            position: 'relative',
-            transition: isDragging ? null : '0.2s',
-            userSelect: 'none',
-            width: this.getWidth(),
-          }),
-          ...style,
-        }}
+        style={{ display: 'inline-block' }}
+        tabIndex="0"
+        role="switch"
+        onKeyDown={this.handleOnKeyDown}
       >
-        <span 
-          onClick={this.handleHandleClick}
-          onMouseDown={this.handleMouseDown}
-          style={prefixStyle({
-            backgroundColor: this.getHandleColor(),
-            borderRadius: '100%',
-            boxShadow: '0 1px 3px rgba(0, 0, 0, 0.4)',
-            cursor: this.getHandleCursor(),
-            display: 'inline-block',
-            height: this.getHandleLength(),
-            left: this.getOffset(),
-            position: 'absolute',
-            top: 0,
-            transition: isDragging ? null : '0.2s',
-            width: this.getHandleLength(),
-          })}
-        />
-        <input
-          checked={checked}
-          disabled={disabled}
-          name={name}
-          onChange={this.handleChange}
-          readOnly={readOnly}
+        <span
+          onClick={this.handleClick}
+          onKeyDown={this.handleOnKeyDown}
+          ref={this.setRef}
           style={{
-            display: 'none',
+            ...prefixStyle({
+              backgroundColor: color,
+              border: `1px solid ${borderColor}`,
+              borderRadius: this.getHeight() / 2,
+              boxShadow: `inset 0 0 0 ${this.getOffset()}px ${borderColor}`,
+              boxSizing: 'border-box',
+              display: 'inline-block',
+              height: this.getHeight(),
+              opacity: this.isDisabled() ? 0.5 : 1,
+              position: 'relative',
+              transition: isDragging ? null : '0.2s',
+              userSelect: 'none',
+              width: this.getWidth()
+            }),
+            ...style
           }}
-          type="checkbox"
-        />
-      </span>
+        >
+          <span
+            onClick={this.handleHandleClick}
+            onMouseDown={this.handleMouseDown}
+            style={prefixStyle({
+              backgroundColor: this.getHandleColor(),
+              borderRadius: '100%',
+              boxShadow: '0 1px 3px rgba(0, 0, 0, 0.4)',
+              cursor: this.getHandleCursor(),
+              display: 'inline-block',
+              height: this.getHandleLength(),
+              left: this.getOffset(),
+              position: 'absolute',
+              top: 0,
+              transition: isDragging ? null : '0.2s',
+              width: this.getHandleLength()
+            })}
+          />
+          <input
+            checked={checked}
+            disabled={disabled}
+            name={name}
+            onChange={this.handleChange}
+            readOnly={readOnly}
+            style={{
+              display: 'none'
+            }}
+            type="checkbox"
+          />
+        </span>
+      </div>
     );
   }
 
